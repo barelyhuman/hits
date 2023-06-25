b950274f3895b04d3862c61f470b61b510ae3cca
Reaper Gelera <ahoy@barelyhuman.dev>
2023-06-25 17:35:14 +0530

    fix: remove shiki due to dynamic require issues

--- routes/index.js
+++ routes/index.js
@@ -2,27 +2,27 @@ import BaseLayout from '../layouts/base-layout'
 import LandingPage from '../pages/landing.page'
 import { marked } from 'marked'
 import { gfmHeadingId } from 'marked-gfm-heading-id'
-import shiki from 'shiki'
+// import shiki from 'shiki'
 
 export default defineEventHandler(async ev => {
   const md = await useStorage('assets:server').getItem('home.md')
-  const highlighter = await shiki.getHighlighter({
-    theme: 'min-light',
-  })
+  // const highlighter = await shiki.getHighlighter({
+  //   theme: 'min-light',
+  // })
 
-  const renderer = {
-    code(code, infostring, escaped) {
-      return highlighter.codeToHtml(code, { lang: infostring })
-    },
-  }
+  // const renderer = {
+  //   code(code, infostring, escaped) {
+  //     return highlighter.codeToHtml(code, { lang: infostring })
+  //   },
+  // }
 
   marked.use(gfmHeadingId())
   marked.use({
     mangle: false,
   })
-  marked.use({
-    renderer,
-  })
+  // marked.use({
+  //   renderer,
+  // })
 
   const html = marked(md)
   return renderComponent(
