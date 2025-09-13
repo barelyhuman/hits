ba81696c8b515830f76262b7940fa50dcd01e993
reaper <ahoy@barelyhuman.dev>
2025-09-13 06:17:32 +0530

    feat: enhance URL handling and parsing

--- src/main.cr
+++ src/main.cr
@@ -1,4 +1,5 @@
 require "kemal"
+require "uri"
 require "./hit-repo"
 
 db = DB.open "sqlite3://./data.db"
@@ -8,15 +9,59 @@ macro renderer(filename)
   render "src/views/#{ {{filename}} }.ecr", "src/views/layouts/base.ecr"
 end
 
+# Utils
+
+def getSumOfAll(hitRepo : HitsRepo, uriStr : String) : Int
+  urls = [] of String
+
+  if uriStr.starts_with?("http://") || uriStr.starts_with?("https://")
+    uri_inst = URI.parse uriStr
+
+    uri_inst.scheme = "http"
+    urls.push(uri_inst.to_s)
+
+    uri_inst.scheme = "https"
+    urls.push(uri_inst.to_s)
+
+    urls.push(uri_inst.to_s.gsub("http://", "").gsub("https://", ""))
+  else
+    uri_inst = URI.parse uriStr
+    urls.push("http://#{uri_inst.to_s}")
+    urls.push("https://#{uri_inst.to_s}")
+    urls.push(uriStr)
+  end
+
+  count_int = 0
+  urls.each do |url|
+    count = hitRepo.get_hits_by_url url
+    count_int += count.to_s.to_i
+  end
+
+  return count_int
+end
+
+def parseURL(uriStr : String) : String
+  normalised_uri = uriStr
+  if uriStr.starts_with?("http://") || uriStr.starts_with?("https://")
+    uri_instance = URI.parse uriStr
+    return "#{uri_instance.hostname}:#{uri_instance.port}"
+  end
+  uri_instance = URI.parse "http://#{uriStr}"
+  return "#{uri_instance.hostname}:#{uri_instance.port}"
+end
+
+# Routes
+
 get "/" do |env|
   renderer "index"
 end
 
 get "/hit" do |env|
   url = env.params.query["url"]
+  parsedURL = parseURL url
   env.response.content_type = "application/json"
   begin
-    hitRepo.add_site url
+    hitRepo.add_site parsedURL
     %({"success": true})
   rescue ex
     puts ex
@@ -26,8 +71,8 @@ end
 
 get "/hits" do |env|
   url = env.params.query["url"]
-  count = hitRepo.get_hits_by_url url
-
+  parsedURL = parseURL url
+  count = getSumOfAll(hitRepo, url)
   env.response.content_type = "application/json"
   %({"count": #{count}})
 end
